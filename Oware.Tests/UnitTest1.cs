ad87c8dd93ec3f407478ec9ad32e94b6f94c648e
Thelma <tadichits@gmail.com>
2021-05-18 11:27:43 +0200

    the tests have been commited

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -12,6 +12,26 @@ namespace Oware.Tests
         [Test]
         public void Test1()
         {
+            // Arrange
+            House h = new House(0, 0);
+            for (int i = 0; i < 3; i++)
+            {
+                h.AddSeedInPot(new Seed());
+            }
+            // Act
+            h.ResetHouse();
+            // Assert
+            Assert.AreEqual(4, h.GetCount(), "adding new seeds increases the pots new seed number");
+            
+        }
+
+        [Test]
+
+        public void Test2()
+        {
+            // Arrange
+            // Act
+            // Assert
             Assert.Pass();
         }
     }
